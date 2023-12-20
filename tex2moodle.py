0275664aa17c7e46fd1bdc61b8437ae91bea214d
Ravi Rajani <ravi.inajar@gmail.com>
2023-12-20 07:14:41 +0000

    add html conversion

--- tex2moodle.py
+++ tex2moodle.py
@@ -2,17 +2,20 @@ import sys
 import re
 
 filename = sys.argv[1]
-output = ''
+output = '<p>'
 with open(filename, encoding='utf-8') as f:
     for line in f:
         if line.startswith('\\[') or line.startswith('\\]'):
             output += '\n$$\n'
         elif not line.startswith('\\'):
             if line.startswith('\n'):
-                output += line + '\n'
+                output += '</p>\n<p>'
             else:
                 output += re.sub(r'\\textit\{.*?\}', r'$$\g<0>$$',
                                  line[:-1].replace('$','$$')
-                                    .replace('``','"')
-                                    .replace("''",'"')) + ' '
-print(output)
+                                    .replace('``','&ldquo;')
+                                    .replace("''",'&rdquo;')
+                                    .replace('---','&mdash;')
+                                    .replace('--','&ndash;')
+                                )
+print(output + '</p>')
